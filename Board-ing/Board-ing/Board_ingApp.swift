912d75341be4408752c868e04193eff91865a5f1
Bell <quf417@gmail.com>
2024-06-17 11:19:41 +0900

    init Project

---
+++ Board-ing/Board-ing/Board_ingApp.swift
@@ -0,0 +1,19 @@
+//
+//  Board_ingApp.swift
+//  Board-ing
+//
+//  Created by Byeol Kim on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Board_ingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+            
+        }
+    }
+}
---
+++ Board-ing/Board-ing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Board-ing
+//
+//  Created by Byeol Kim on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
