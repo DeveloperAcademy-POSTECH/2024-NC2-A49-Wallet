ea3aa69dd17155b26e62c27e24ed03c01c3911ab
Bell <quf417@gmail.com>
2024-06-17 11:42:30 +0900

    test 2

--- Board-ing/Board-ing/ContentView.swift
+++ Board-ing/Board-ing/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("천재가되겠어!")
+            Text("반드시!!")
         }
         .padding()
     }
